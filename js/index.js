faf95c6ad16f330699104def9c21674be13f9623
Tirthrshah007 <tirth.shah@bacancy.com>
2021-06-14 18:38:28 +0530

    Fixes

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
-$(document).ready(function(){
-  
+function enablePopOver() {
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  });
+}
+
+function adjustNavbar() {
   const activeElement = $(".horizontal-navbar .active").first();
   const activeElementLeftOffset = activeElement.offset().left;
   const activeElementParentLeftOffset = activeElement.parent().offset().left;
   $(".horizontal-navbar").scrollLeft(
     activeElementLeftOffset - activeElementParentLeftOffset
   );
+}
+
+$(document).ready(function(){
+  enablePopOver();
+  adjustNavbar();
 });
