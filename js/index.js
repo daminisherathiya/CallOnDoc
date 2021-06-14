01be0e89092275976c9973c7fd63dcdaf20eeb44
Tirthrshah007 <tirth.shah@bacancy.com>
2021-06-14 19:19:31 +0530

    Popover related changes

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
+var moreInfoTooltip;
+
 function enablePopOver() {
   var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
   var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
     return new bootstrap.Popover(popoverTriggerEl)
   });
+  moreInfoTooltip = popoverList[0];
 }
 
 function adjustNavbar() {
@@ -12,6 +15,13 @@ function adjustNavbar() {
   $(".horizontal-navbar").scrollLeft(
     activeElementLeftOffset - activeElementParentLeftOffset
   );
+}
+
+function showMoreInfoPopOver() {
+  moreInfoTooltip.show();
+  $(".popover-body").on("click", function(){
+    moreInfoTooltip.hide();
+  });
 }
 
 $(document).ready(function(){
